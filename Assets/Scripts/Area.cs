ac76a00fa7a59df8843962289ab154a65b6fb712
DaniilOykin <71172456+DaniilOykin@users.noreply.github.com>
2022-03-01 18:45:54 +0300

    1

--- Assets/Scripts/Area.cs
+++ Assets/Scripts/Area.cs
@@ -1,30 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Mapbox.Utils;
 using Mapbox.Unity.Map;
 using Mapbox.Unity.Utilities;
 
 public class Area : MonoBehaviour
 {
-	public Area(AbstractMap _map, Vector2d _location, float _spawnScale)
-    {
-		map = _map;
-		location = _location;
-		spawnScale = _spawnScale;
-    }
-
-	[Geocode]
-	Vector2d location;
-
+	GameObject area;
 	AbstractMap map;
+
+	private void Awake()
+    {
+		GameObject area = gameObject;
+		AbstractMap map = area.GetComponentInParent<AbstractMap>();
+	}
+
+	public Vector2d location;
+
+	public string areaType;
 
 	float spawnScale = 100f;
 
+	public void UseZone()
+    {
+		if (areaType == "PickUp")
+        {
+			//map.GetComponent<AreaSpawner>().area;
+			//map.GetComponent<AreaSpawner>().SpawnArea("Drop");
+        }
+        else
+        {
+			SceneManager.LoadScene("CameraNew");
+        }
+    }
+
 	private void Update()
 	{
-		GetComponentInParent<Area>().transform.localPosition = map.GeoToWorldPosition(location, true) + new Vector3(0, 0.1f, 0);
+		if (area == null) { return; }
+		area.transform.position = map.GeoToWorldPosition(location, true);
+		area.transform.localPosition += new Vector3(1, 1, 1);
 		//spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale); // относительное отображение
-		GetComponentInParent<Area>().transform.localScale = new Vector3(spawnScale * map.transform.lossyScale.x, spawnScale * map.transform.lossyScale.y, spawnScale * map.transform.lossyScale.z);
+		area.transform.localScale = new Vector3(spawnScale * map.transform.lossyScale.x, spawnScale * map.transform.lossyScale.y, spawnScale * map.transform.lossyScale.z);
 	}
 }
--- Assets/Scripts/AreaSpawner.cs
+++ Assets/Scripts/AreaSpawner.cs
@@ -10,7 +10,7 @@ using Mapbox.Unity.Utilities;
 public class AreaSpawner : MonoBehaviour
 {
 	[SerializeField]
-	AbstractMap _map;
+	AbstractMap map;
 
 	[SerializeField]
 	GameObject player;
@@ -19,21 +19,30 @@ public class AreaSpawner : MonoBehaviour
 	InputField order;
 
 	[SerializeField]
-	float _spawnScale = 100f;
+	GameObject area;
 
-	[Geocode]
-	Vector2d[] _locations;
+	[SerializeField]
+	float spawnScale = 100f;
 
-	public void SpawnArea()
-	{
+	Vector2d[] locations;
+
+    public void Update()
+    {
 		int areaCount = int.Parse(order.text);
-		_locations = new Vector2d[areaCount];
+	}
+
+    public void SpawnArea(string areaType, int areaCount, int drugCount)
+	{
+		locations = new Vector2d[areaCount];
 		for (int i = 0; i < areaCount; i++)
 		{
-			_locations[i] = _map.WorldToGeoPosition(player.transform.position + new Vector3(Random.Range(-100,100), 0, Random.Range(-100, 100)));
-			var instance = new Area(_map, _locations[i], _spawnScale);
-			instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
-			instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+			locations[i] = map.WorldToGeoPosition(player.transform.position + new Vector3(Random.Range(-100,100), 0, Random.Range(-100, 100)));
+			var instance = Instantiate(area);
+			instance.GetComponent<Area>().location = locations[i];
+			instance.transform.localPosition = map.GeoToWorldPosition(locations[i], true) + new Vector3(0, 1, 0);
+			instance.transform.localScale = new Vector3(spawnScale, spawnScale, spawnScale);
+			instance.transform.SetParent(map.transform);
+			instance.GetComponent<Area>().areaType = areaType;
 		}
 	}
 }
